721e2d38a9653b19dff4803c14e633382fcbe737
taipeihugo <taipeihugo@users.noreply.github.com>
2024-10-09 15:54:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -103,17 +103,6 @@ def handle_message(event):
                     ImageMessage(original_content_url=img_url, preview_image_url=img_url)
                 )
             )
-        elif text == '地震':
-            '''
-            reply = earth_quake()  # 執行地震查詢函式
-            text_message = TextSendMessage(text=reply[0])  # 傳送地震報告內容
-            line_bot_api.reply_message(event.reply_token, text_message)
-            
-            if reply[1]:  # 如果有地震圖片，則回傳
-                json_data = json.loads(event)
-                user_id = json_data['events'][0]['source']['userId']  # 取得使用者 ID
-                line_bot_api.push_message(user_id, ImageSendMessage(original_content_url=reply[1], preview_image_url=reply[1]))
-            '''
         else:
             line_bot_api.reply_message(
                 ReplyMessageRequest(
@@ -135,7 +124,7 @@ def create_rich_menu_1():
                     width=833,
                     height=843
                 ),
-                action=MessageAction(text='雷達回波')
+                action=MessageAction(text='文字')
             ),
             RichMenuArea(
                 bounds=RichMenuBounds(
@@ -144,7 +133,7 @@ def create_rich_menu_1():
                     width=833,
                     height=843
                 ),
-                action=MessageAction(text='文字')
+                action=MessageAction(text='雷達回波')
             ),
             RichMenuArea(
                 bounds=RichMenuBounds(
@@ -208,130 +197,6 @@ def create_rich_menu_1():
 
         line_bot_api.set_default_rich_menu(rich_menu_id)
 
-# 地震查詢功能，整合中央氣象局地震資料的 API
-def earth_quake():
-    result = []
-    code = weath_api  # 你的天氣 API 授權碼
-    try:
-        # 小區域地震
-        url1 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0016-001?Authorization={code}'
-        req1 = requests.get(url1)
-        data1 = req1.json()
-        eq1 = data1['records']['Earthquake'][0]
-        t1 = eq1['EarthquakeInfo']['OriginTime']
-
-        # 顯著有感地震
-        url2 = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/E-A0015-001?Authorization={code}'
-        req2 = requests.get(url2)
-        data2 = req2.json()
-        eq2 = data2['records']['Earthquake'][0]
-        t2 = eq2['EarthquakeInfo']['OriginTime']
-
-        # 使用最新的地震資料
-        result = [eq1['ReportContent'], eq1['ReportImageURI']]  # 使用小區域地震
-        if t2 > t1:
-            result = [eq2['ReportContent'], eq2['ReportImageURI']]  # 如果顯著有感地震時間較近
-    except Exception as e:
-        print(e)
-        result = ['抓取失敗...', '']  # 如果發生錯誤，返回失敗訊息
-    return result
-
-# 取得某個地點的氣象資訊，整合氣象預報與空氣品質
-def weather(address):
-    result = {}
-    # 即時天氣
-    try:
-        url = [f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization={code}',
-            f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization={code}']
-        for item in url:
-            req = requests.get(item)   # 爬取目前天氣網址的資料
-            data = req.json()
-            station = data['records']['Station']
-            for i in station:
-                city = i['GeoInfo']['CountyName']
-                area = i['GeoInfo']['TownName']
-                if not f'{city}{area}' in result:
-                    weather = i['WeatherElement']['Weather']
-                    temp = i['WeatherElement']['AirTemperature']
-                    humid = i['WeatherElement']['RelativeHumidity']
-                    result[f'{city}{area}'] = f'目前天氣狀況「{weather}」，溫度 {temp} 度，相對濕度 {humid}%！'
-    except:
-        pass
-
-    # 氣象預報
-    api_list = {"宜蘭縣":"F-D0047-001","桃園市":"F-D0047-005","新竹縣":"F-D0047-009","苗栗縣":"F-D0047-013",
-        "彰化縣":"F-D0047-017","南投縣":"F-D0047-021","雲林縣":"F-D0047-025","嘉義縣":"F-D0047-029",
-        "屏東縣":"F-D0047-033","臺東縣":"F-D0047-037","花蓮縣":"F-D0047-041","澎湖縣":"F-D0047-045",
-        "基隆市":"F-D0047-049","新竹市":"F-D0047-053","嘉義市":"F-D0047-057","臺北市":"F-D0047-061",
-        "高雄市":"F-D0047-065","新北市":"F-D0047-069","臺中市":"F-D0047-073","臺南市":"F-D0047-077",
-        "連江縣":"F-D0047-081","金門縣":"F-D0047-085"}
-    for name in api_list:
-        if name in address:
-            city_id = api_list[name]
-    t = time.time()
-    t1 = time.localtime(t+28800)
-    t2 = time.localtime(t+28800+10800)
-    now = time.strftime('%Y-%m-%dT%H:%M:%S',t1)
-    now2 = time.strftime('%Y-%m-%dT%H:%M:%S',t2)
-    url = f'https://opendata.cwa.gov.tw/api/v1/rest/datastore/{city_id}?Authorization={code}&elementName=WeatherDescription&timeFrom={now}&timeTo={now2}'
-    req = requests.get(url)   # 取得主要縣市預報資料
-    data = req.json()         # json 格式化訊息內容
-    location = data['records']['locations'][0]['location']
-    city = data['records']['locations'][0]['locationsName']
-    for item in location:
-        try:
-            area = item['locationName']
-            note = item['weatherElement'][0]['time'][0]['elementValue'][0]['value']
-            if not f'{city}{area}' in result:
-                result[f'{city}{area}'] = ''
-            else:
-                result[f'{city}{area}'] = result[f'{city}{area}'] + '。\n\n'
-            result[f'{city}{area}'] = result[f'{city}{area}'] + '未來三小時' + note
-        except:
-            pass
-
-    # 空氣品質
-    try:
-        url = 'https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
-        req = requests.get(url)
-        data = req.json()
-        records = data['records']
-        for item in records:
-            county = item['county']      # 縣市
-            sitename = item['sitename']  # 區域
-            name = f'{county}{sitename}'
-            aqi = int(item['aqi'])       # AQI 數值
-            aqi_status = ['良好','普通','對敏感族群不健康','對所有族群不健康','非常不健康','危害']
-            msg = aqi_status[aqi//50]    # 除以五十之後無條件捨去，取得整數
-
-            for k in result:
-                if name in k:
-                    result[k] = result[k] + f'\n\nAQI：{aqi}，空氣品質{msg}。'
-    except:
-        pass
-
-    output = '找不到氣象資訊'
-    for i in result:
-        if i in address: # 如果地址裡存在 key 的名稱
-            output = f'「{address}」{result[i]}'
-            break
-    return output
-
-def cctv(msg):
-    try:
-        output = ''
-        camera_list = {
-            '夢時代':'https://cctv1.kctmc.nat.gov.tw/27e5c086/',
-            '鼓山渡輪站':'https://cctv3.kctmc.nat.gov.tw/ddb9fc98/',
-            '中正交流道':'https://cctv3.kctmc.nat.gov.tw/166157d9/',
-            '五福愛河':'https://cctv4.kctmc.nat.gov.tw/335e2702/'
-        }
-        for item in camera_list:
-            if msg == item:
-                output = camera_list[msg]
-    except Exception as e:
-        print(e)
-    return output
 
 
 
