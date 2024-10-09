ed8f213fc3571dd1b9823ee53a16b6d3d1f5a3b7
taipeihugo <taipeihugo@users.noreply.github.com>
2024-10-09 16:00:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,18 +10,15 @@ from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent
 )
-from linebot.models import *
 import os
 import requests
 import json
-import time
 
 app = Flask(__name__)
 
 
 configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
 line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-weath_api = os.getenv('WEATHER_API')
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -40,6 +37,92 @@ def callback():
         abort(400)
 
     return 'OK'
+
+def create_rich_menu_1():
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_blob_api = MessagingApiBlob(api_client)
+
+        areas = [
+            RichMenuArea(
+                bounds=RichMenuBounds(
+                    x=0,
+                    y=0,
+                    width=833,
+                    height=843
+                ),
+                action=MessageAction(text='文字')
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(
+                    x=834,
+                    y=0,
+                    width=833,
+                    height=843
+                ),
+                action=MessageAction(text='表情符號')
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(
+                    x=1663,
+                    y=0,
+                    width=834,
+                    height=843
+                ),
+                action=MessageAction(text='貼圖')
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(
+                    x=0,
+                    y=843,
+                    width=833,
+                    height=843
+                ),
+                action=MessageAction(text='圖片')
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(
+                    x=834,
+                    y=843,
+                    width=833,
+                    height=843
+                ),
+                action=MessageAction(text='位置')
+            ),
+            RichMenuArea(
+                bounds=RichMenuBounds(
+                    x=1662,
+                    y=843,
+                    width=834,
+                    height=843
+                ),
+                action=MessageAction(text='F')
+            )
+        ]
+
+        rich_menu_to_create = RichMenuRequest(
+            size=RichMenuSize(
+                width=2500,
+                height=1686,
+            ),
+            selected=True,
+            name="圖文選單1",
+            chat_bar_text="查看更多資訊",
+            areas=areas
+        )
+
+        rich_menu_id = line_bot_api.create_rich_menu(
+            rich_menu_request=rich_menu_to_create
+        ).rich_menu_id
+
+        with open('static/richmenu-1.jpg', 'rb') as image:
+            line_bot_blob_api.set_rich_menu_image(
+                rich_menu_id=rich_menu_id,
+                body=bytearray(image.read()),
+                _headers={'Content-Type': 'image/jpeg'}
+            )
+
+        line_bot_api.set_default_rich_menu(rich_menu_id)
 
 
 @line_handler.add(MessageEvent, message=TextMessageContent)
@@ -102,94 +185,6 @@ def handle_message(event):
                     messages=[TextMessage(text=event.message.text)]
                 )
             )
-
-def create_rich_menu_1():
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        line_bot_blob_api = MessagingApiBlob(api_client)
-
-        areas = [
-            RichMenuArea(
-                bounds=RichMenuBounds(
-                    x=0,
-                    y=0,
-                    width=833,
-                    height=843
-                ),
-                action=MessageAction(text='文字')
-            ),
-            RichMenuArea(
-                bounds=RichMenuBounds(
-                    x=834,
-                    y=0,
-                    width=833,
-                    height=843
-                ),
-                action=MessageAction(text='雷達回波')
-            ),
-            RichMenuArea(
-                bounds=RichMenuBounds(
-                    x=1663,
-                    y=0,
-                    width=834,
-                    height=843
-                ),
-                action=MessageAction(text='表情符號')
-            ),
-            RichMenuArea(
-                bounds=RichMenuBounds(
-                    x=0,
-                    y=843,
-                    width=833,
-                    height=843
-                ),
-                action=MessageAction(text='貼圖') # 圖片
-            ),
-            RichMenuArea(
-                bounds=RichMenuBounds(
-                    x=834,
-                    y=843,
-                    width=833,
-                    height=843
-                ),
-                action=MessageAction(text='位置')
-            ),
-            RichMenuArea(
-                bounds=RichMenuBounds(
-                    x=1662,
-                    y=843,
-                    width=834,
-                    height=843
-                ),
-                action=MessageAction(text='F')
-            )
-        ]
-
-        rich_menu_to_create = RichMenuRequest(
-            size=RichMenuSize(
-                width=2500,
-                height=1686,
-            ),
-            selected=True,
-            name="圖文選單1",
-            chat_bar_text="查看更多資訊",
-            areas=areas
-        )
-
-        rich_menu_id = line_bot_api.create_rich_menu(
-            rich_menu_request=rich_menu_to_create
-        ).rich_menu_id
-
-        with open('static/richmenu-1.jpg', 'rb') as image:
-            line_bot_blob_api.set_rich_menu_image(
-                rich_menu_id=rich_menu_id,
-                body=bytearray(image.read()),
-                _headers={'Content-Type': 'image/jpeg'}
-            )
-
-        line_bot_api.set_default_rich_menu(rich_menu_id)
-
-
 
 
 create_rich_menu_1()
